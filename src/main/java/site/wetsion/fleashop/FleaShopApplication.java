bd3efa4307405255d978a246f349ea2a9c37e5a6
魏鑫 <weixin@dxy.cn>
2019-09-11 14:36:52 +0800

    init

---
+++ src/main/java/site/wetsion/fleashop/FleaShopApplication.java
@@ -0,0 +1,13 @@
+package site.wetsion.fleashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleaShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FleaShopApplication.class, args);
+    }
+
+}
